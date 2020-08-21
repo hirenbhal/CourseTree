8386fdb5ac3b6d42c0a4a1c14c1fd36669946df9
hirenbhal <iamhiren5@gmail.com>
2020-08-21 20:26:20 +0530

    final

--- index.js
+++ index.js
@@ -256,6 +256,8 @@ app.get('/:courseName', insureAuthenticated, (req, res)=>{
   }
 });
 
-app.listen(3000, ()=>{
-  console.log('App is running on port 3000 Succesfully');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
